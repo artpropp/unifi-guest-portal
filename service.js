97f5390b513d8090700a0367e96709033ae1d53c
Artur Propp <artur.propp@me.com>
2019-05-25 18:30:17 +0200

    Update dependendies

--- service.js
+++ service.js
@@ -2,10 +2,18 @@ const express = require('express')
 const logger = require('./utils/logger')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//     res.send('It works!')
+//     logger.info(req.url + ' query: ' + req.query.page)
+//     logger.info('request root page')
+// })
+
+
+app.use((req, res) => {
     res.send('It works!')
+    logger.info(req.originalUrl)
     logger.info('request root page')
 })
 
@@ -13,7 +21,7 @@ app.listen(port, () => {
     logger.info(`Service is up and listening on ${port}!`)
 })
 
-process.on('SIGINT', () => {
+process.on('SIGINT', () => { 
     console.log('\r')
     logger.info('Service shutdown through app termination!')
     process.exit(0)
